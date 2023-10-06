aebb3b66b56875dbf78aa5c5c5b62f30b12d5def
lauriano carlos <laurianocarlos@hotmail.com>
2023-10-06 17:34:30 -0300

    Alterações no pagina não logada e criacao do arquivo da tela de login

---
+++ src/main/java/br/com/clinic/servlet/LoginServlet.java
@@ -0,0 +1,18 @@
+package br.com.clinic.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Carregue a página de login
+        req.getRequestDispatcher("/login.html").forward(req, resp);
+    }
+}
